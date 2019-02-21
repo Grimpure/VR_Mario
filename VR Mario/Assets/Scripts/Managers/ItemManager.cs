3fcfec394215c17372cd2001826e2e33ffb01b05
Unknown <b.muijsenberg@sintlucasedu.nl>
2019-02-21 15:48:01 +0100

    PowerUp picker finished

---
+++ VR Mario/Assets/Scripts/Managers/ItemManager.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Managers;
+
+namespace Items
+{
+    public class ItemManager : MonoBehaviour
+    {
+
+        public GameObject item;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                item = null;
+
+                UIManager.Instance.ChangeItem("");
+            }
+        }
+
+        public void SetItem(GameObject g)
+        {
+            if (!item)
+            {
+                StartCoroutine(RouletteCoroutine(g));
+            }
+        }
+
+        IEnumerator RouletteCoroutine(GameObject newItem)
+        {
+            if (gameObject.name != "Player")
+            {
+                yield break;
+            }
+            if (UIManager.Instance)
+            {
+                UIManager.Instance.ChangeItem(newItem.name);
+            }
+            yield return new WaitForSeconds(3);
+            item = newItem;
+        }
+    }
+}
+
---
+++ VR Mario/Assets/Scripts/PowerUpRandomizer.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Managers;
+
+namespace Items
+{
+    public class PowerUpRandomizer : MonoBehaviour
+    {
+        GameObject[] bItems;
+        public float bRotateSpeed;
+        public float bRespawnTime;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            bItems = GameManager.Instance.bPowerUpPrefabs;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            transform.Rotate(Vector3.forward * bRotateSpeed * Time.deltaTime);
+        }
+
+        private void Hide()
+        {
+            gameObject.GetComponent<BoxCollider>().enabled = false;
+            gameObject.GetComponent<Renderer>().enabled = false;
+
+            //GameObject particles = Resources.Load<GameObject>("Prefabs/ItemBoxParticles");
+            //particles = Instantiate(particles, transform.position, transform.rotation) as GameObject;
+        }
+
+        private void Show()
+        {
+            gameObject.GetComponent<BoxCollider>().enabled = true;
+            gameObject.GetComponent<Renderer>().enabled = true;
+        }
+
+        void OnTriggerEnter(Collider coll)
+        {
+            if (coll.gameObject.tag == "Player" || coll.gameObject.tag == "Enemy")
+            {
+                ItemManager itemMgr = coll.gameObject.GetComponentInParent<ItemManager>();
+
+                if (itemMgr)
+                {
+                    int value = Random.Range(0, bItems.Length);
+                    itemMgr.SetItem(bItems[value]);
+                }
+
+                Hide();
+                StartCoroutine(WaitForRespawn());
+            }
+        }
+
+        IEnumerator WaitForRespawn()
+        {
+            yield return new WaitForSeconds(bRespawnTime);
+            Show();
+        }
+    }
+}
+
