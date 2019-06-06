5dfc77fff6d28291b8eccfe884a03f2afdb153da
Mathijs van Dinther <ma.van.dinther@sintlucasedu.nl>
2019-06-06 14:10:59 +0200

    Relocated files to gitignore and fixed wiimote

---
+++ Assets/Scripts/CheckWiiMoteTilt.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using WiimoteApi;
+
+public class CheckWiiMoteTilt : MonoBehaviour
+{
+
+    public WiimoteDemo wmd;
+    public Vector3 wiiMoteRotation;
+    public Slider debugSlider;
+
+    public float steering;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        wiiMoteRotation = wmd.wmpOffset;
+        //debugSlider.value = wmd.wmpOffset.y;
+
+        //debugSlider.value = wmd.wmpOffset.y;
+        
+        steering = wmd.wmpOffset.y / 30;
+        debugSlider.value = steering;
+        if (steering >= 1)
+        {
+            steering = 1;
+        }
+        if (steering <= -1)
+        {
+            steering = -1;
+        }
+    }
+}
